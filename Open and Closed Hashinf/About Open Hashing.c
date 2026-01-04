e3c7b91a2417f277cbdf864aa0fe48e2e69de481
alyssaaaajo <joannaalyssas.mondelo@gmail.com>
2026-01-04 22:11:39 +0800

    Create About Open Hashing.c

---
+++ Open and Closed Hashinf/About Open Hashing.c
@@ -0,0 +1,8 @@
+OPEN HASHING (Separate Chaining)
+- Also known as separate chaining
+- Each table index stores a linked list of elements
+- All collided keys are stored in the same chain
+- Hash function determines the index only
+- No probing is required
+- Deletion is simple and does not affect other keys
+- Performance depends on chain length
